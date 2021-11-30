28b75fcb81f480e8023fd8f4ae12434daf3c2c4a
BarB93 <barovskiyboris@gmail.com>
2021-11-30 19:43:19 +0300

    added support ie11

--- src/components/PartnerItem.js
+++ src/components/PartnerItem.js
@@ -3,11 +3,13 @@ import React from 'react'
 const PartnerItem = ({imageURL}) => {
     return (
         <li className='partners__item'>
+          <div className='partners__wrapper'>
             <div className='partners__itemContainer'>
                 <div className='partners__logo'>
                     <img src={imageURL} alt='Logo'/>
                 </div>
             </div>
+          </div>
         </li>
     )
 }
